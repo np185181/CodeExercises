8f7cbd31f6522e925a5e83448048d118df88a9e5
pawalenavratna <117887016+pawalenavratna@users.noreply.github.com>
2022-12-07 15:56:56 +0530

    Add files via upload

---
+++ Servers/Store/App/Src/Model/Public/UserRoles/IUserRolesInfo.cs
@@ -0,0 +1,16 @@
+﻿using Retalix.StoreServices.Model.Infrastructure.DataMovement;
+
+namespace Retalix.StoreServices.Model.UserRoles
+{
+    /// <summary>
+    /// IUserRolesInfo interface for UserRolesInfo class
+    /// </summary>
+    public interface IUserRolesInfo : IMovable
+    {
+        int UserRoleId { get; set; }
+
+        string UserRoleName { get; set; }
+
+        string UserRoleType { get; set; }
+    }
+}
---
+++ Servers/Store/App/Src/Model/Public/UserRoles/UserRolesDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Retalix.StoreServices.Model.UserRoles
+{
+    /// <summary>
+    /// UserRolesDto Class
+    /// </summary>
+    [Serializable]
+    public class UserRolesDto
+    {
+        public virtual int UserRoleId { get; set; }
+        public virtual string UserRoleName { get; set; }
+        public virtual string UserRoleType { get; set; }
+        public virtual string UserRoleDescription { get; set; }
+        public virtual string IsUserRoleActive { get; set; }
+    }
+}
